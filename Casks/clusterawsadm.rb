5e4faade949f4cace55406e70ece5201b59fc289
John Miller <john.miller@d2iq.com>
2021-08-30 22:06:36 -0400

    Update clusterawsadm.rb

--- Casks/clusterawsadm.rb
+++ Casks/clusterawsadm.rb
@@ -1,4 +1,7 @@
-class Clusterawsapi < Formula
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class ClusterApiProviderAws < Formula
   desc "Kubernetes Cluster API Provider AWS provides consistent deployment and day 2 operations of self-managed and EKS Kubernetes clusters on AWS."
   homepage "http://cluster-api-aws.sigs.k8s.io/"
   url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-darwin-amd64"
@@ -11,15 +14,15 @@ class Clusterawsapi < Formula
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-darwin-amd64"
-    sha256 
+    sha256 ""
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-linux-amd64"
-    sha256 :no_check
+    sha256 ""
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-linux-arm64"
-    sha256 :no_check
+    sha256 ""
   end
 
   def install
