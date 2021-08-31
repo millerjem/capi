1ae344d1e0b2898ac68ef399f3fdd47ef361030d
John Miller <john.miller@d2iq.com>
2021-08-30 22:36:43 -0400

    Update clusterawsadm.rb

--- Casks/clusterawsadm.rb
+++ Casks/clusterawsadm.rb
@@ -1,11 +1,6 @@
 cask "clusterawsadm" do
   version "0.7.0"
-  desc "Kubernetes Cluster API Provider AWS provides consistent deployment and day 2 operations of self-managed and EKS Kubernetes clusters on AWS."
-  homepage "http://cluster-api-aws.sigs.k8s.io/"
-  url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-darwin-amd64"
-  sha256 "9dbf25637168e142dea0a0e55afd345a227d0b4bae73a2f39893024df7c0f445"
-  license "Apache-2.0"
-
+ 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-darwin-amd64"
     sha256 "9dbf25637168e142dea0a0e55afd345a227d0b4bae73a2f39893024df7c0f445"
@@ -23,6 +18,10 @@ cask "clusterawsadm" do
     sha256 ""
   end
 
+  desc "Kubernetes Cluster API Provider AWS provides consistent deployment and day 2 operations of self-managed and EKS Kubernetes clusters on AWS."
+  homepage "http://cluster-api-aws.sigs.k8s.io/"
+  license "https://raw.githubusercontent.com/kubernetes-sigs/cluster-api-provider-aws/main/LICENSE"
+  
   def install
     bin.install "clusterawsapi"
   end
