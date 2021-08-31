51ed3c0622691ace2efd3e540685b081261a4688
John Miller <john.miller@d2iq.com>
2021-08-30 23:23:44 -0400

    Update clusterawsadm.rb

--- Formula/clusterawsadm.rb
+++ Formula/clusterawsadm.rb
@@ -5,19 +5,19 @@ class ClusterAWSAdm < Formula
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-darwin-amd64"
+    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v#{version}/clusterawsadm-darwin-amd64"
     sha256 "9dbf25637168e142dea0a0e55afd345a227d0b4bae73a2f39893024df7c0f445"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-darwin-amd64"
+    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v#{version}/clusterawsadm-darwin-amd64"
     sha256 ""
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-linux-amd64"
+    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v#{version}/clusterawsadm-linux-amd64"
     sha256 ""
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-linux-arm64"
+    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v#{version}/clusterawsadm-linux-arm64"
     sha256 ""
   end
 
