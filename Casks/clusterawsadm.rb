1d397eddc4498245c4b1a1bd28b916e4bc51e52b
John Miller <john.miller@d2iq.com>
2021-08-30 22:56:26 -0400

    Update clusterawsadm.rb

--- Casks/clusterawsadm.rb
+++ Casks/clusterawsadm.rb
@@ -18,9 +18,15 @@ cask "clusterawsadm" do
     sha256 "9dbf25637168e142dea0a0e55afd345a227d0b4bae73a2f39893024df7c0f445"
   end
 
-  name "ClusterAWSAdm"
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)$/)
+  end
+  
+  name "clusterawsadm"
   desc "Kubernetes Cluster API Provider AWS provides consistent deployment and day 2 operations of self-managed and EKS Kubernetes clusters on AWS."
   homepage "http://cluster-api-aws.sigs.k8s.io/"
   
-  binary "clusterawsadm-darwin-amd64", target: "clusterawsadm"
+  app: clusterawsadm
+#  binary "clusterawsadm-darwin-amd64", target: "clusterawsadm"
 end
