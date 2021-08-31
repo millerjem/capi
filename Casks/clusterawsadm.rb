8b46f3b62d47b18f33e3e720431f58a8dea87722
John Miller <john.miller@d2iq.com>
2021-08-30 21:52:34 -0400

    Update clusterawsadm.rb

--- Casks/clusterawsadm.rb
+++ Casks/clusterawsadm.rb
@@ -1,22 +1,32 @@
-cask "clusterawsadm" do
-  version "0.7.0"
-  sha256 "9dbf25637168e142dea0a0e55afd345a227d0b4bae73a2f39893024df7c0f445"
-
+class Clusterawsapi < Formula
+  desc "Kubernetes Cluster API Provider AWS provides consistent deployment and day 2 operations of "self-managed" and EKS Kubernetes clusters on AWS."
+  homepage "http://cluster-api-aws.sigs.k8s.io/"
   url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-darwin-amd64"
-  name "clusterawsadm"
-  desc "A command-line tool to bootstrap Kubernetes clusters"
-  homepage "https://cluster-api-aws.sigs.k8s.io/"
+  sha256 "9dbf25637168e142dea0a0e55afd345a227d0b4bae73a2f39893024df7c0f445"
+  license "Apache-2.0"
 
-  binary "clusterawsadm-darwin-amd64", target: "clusterawsadm"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-darwin-amd64"
+    sha256 "9dbf25637168e142dea0a0e55afd345a227d0b4bae73a2f39893024df7c0f445"
+  end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-darwin-amd64"
+    sha256 
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-linux-amd64"
+    sha256 :no_check
+  end
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-linux-arm64"
+    sha256 :no_check
+  end
 
-  depends_on macos: [
-    :catalina,
-    :big_sur,
-  ]
-  
-  depends_on arch: :intel
+  def install
+    bin.install "clusterawsapi"
+  end
 
-  uninstall clusterawsadm: [
-    ""
-  ]
+  test do
+    system "#{bin}/clusterawsapi --version"
+  end
 end
