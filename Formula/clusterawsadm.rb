00a699a349ace46585d3ffcfe503691d9f2a9841
John Miller <john.miller@d2iq.com>
2021-08-30 23:23:13 -0400

    Update clusterawsadm.rb

--- Formula/clusterawsadm.rb
+++ Formula/clusterawsadm.rb
@@ -1,9 +1,7 @@
-cask "clusterawsadm" do
+class ClusterAWSAdm < Formula
   version 0.7.0
   desc "Kubernetes Cluster API Provider AWS provides consistent deployment and day 2 operations of self-managed and EKS Kubernetes clusters on AWS."
   homepage "http://cluster-api-aws.sigs.k8s.io/"
-  url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.7.0/clusterawsadm-darwin-amd64"
-  sha256 "9dbf25637168e142dea0a0e55afd345a227d0b4bae73a2f39893024df7c0f445"
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.intel?
